844ca1da1a23b6a47de575d26d6117a2accc8869
blackthecoder <31772179+blackthecoder@users.noreply.github.com>
2019-08-21 23:03:36 +0700

    ปรับปรุง unit test โดยเพิ่มการอ่านค่าที่มันทำการปริ้นออกมา

--- mentalmath/interface.py
+++ mentalmath/interface.py
@@ -1,7 +1,11 @@
+GUESSING_TEMPLATE = '{first_number} {operator} {second_number} = ?: '
+
+
 def display_question_and_return_number_from_user(first_number, second_number, operator):
     for i in range(3):
         try:
-            guess = int(input(f'{first_number} {operator} {second_number} = ?: '))
+            guess = int(input(GUESSING_TEMPLATE.format(first_number=first_number, second_number=second_number,
+                                                       operator=operator)))
             return guess
         except ValueError as e:
             print('Please type a valid number !!!')
--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,15 +1,20 @@
 import unittest
 from unittest.mock import patch
-from mentalmath.interface import display_question_and_return_number_from_user
+from mentalmath.interface import display_question_and_return_number_from_user, GUESSING_TEMPLATE
 
 
 class TestInterface(unittest.TestCase):
 
-    def test_type_valid_number_to_display_question_and_return_number_from_user(self):
-        with patch('builtins.input', return_value='10'):
-            display_question_and_return_number_from_user('10', '10', '+')
+    @patch('builtins.input', return_value='10')
+    def test_type_valid_number_to_guessing(self, mock_input):
+        first_number = '10'
+        second_number = '16'
+        operator = '+'
+        display_question_and_return_number_from_user(first_number, second_number, operator)
+        mock_input.assert_called_with(GUESSING_TEMPLATE.format(first_number=first_number,
+                                      second_number=second_number, operator=operator))
 
-    def test_get_exception_when_type_invalid_number_to_display_question_and_return_number_from_user(self):
+    def test_get_exception_when_type_invalid_number_to_guessing(self):
         with patch('builtins.input', return_value='a'):
             self.assertRaises(ValueError, display_question_and_return_number_from_user, '10', '10', '+')
 
